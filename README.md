58c654ff64d0fa84b29ead2764d0c1f7256057d6
leavism <ghuydang@gmail.com>
2023-04-02 00:01:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<img align="right" style="border-radius:1rem;" src="./lovelace-profile.png" height="200" width="200">
+
+# Lovelace
+A Discord bot for the [ACM](https://www.acm.org) student chapter at [SFSU](https://www.sfsu.edu). Our bot is lovingly named after [Ada Lovelace](https://findingada.com/about/who-was-ada/), often referred to as "the first computer programmer."
+
+Lovelace is written in TypeScript and built off the [SapphireJS](https://www.sapphirejs.dev) framework and [Discord.js](https://discord.js.org) library. She serves to make moderating and running our Discord server a little bit easier.
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import './lib/setup';
+import { LogLevel, SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits, Partials } from 'discord.js';
+
+const client = new SapphireClient({
+	defaultPrefix: '!',
+	caseInsensitiveCommands: true,
+	logger: {
+		level: LogLevel.Debug
+	},
+	shards: 'auto',
+	intents: [
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.MessageContent,
+	],
+	partials: [Partials.Channel],
+	loadMessageCommandListeners: true
+});
+
+const main = async () => {
+	try {
+		client.logger.info('Logging in');
+		await client.login();
+		client.logger.info('logged in');
+	} catch (error) {
+		client.logger.fatal(error);
+		client.destroy();
+		process.exit(1);
+	}
+};
+
+main();
