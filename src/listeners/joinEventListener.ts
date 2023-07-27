4bc4b997020507ac1afe6ba52557004c0d8ce04a
vinr567 <vinr567@gmail.com>
2023-07-27 15:55:44 -0700

    Modified to send specifically to greet channel

--- src/listeners/joinEventListener.ts
+++ src/listeners/joinEventListener.ts
@@ -1,5 +1,5 @@
 import { Command, Listener } from "@sapphire/framework";
-import { ChatInputCommandInteraction, Client, CommandInteraction } from "discord.js";
+import { ChatInputCommandInteraction, Client, CommandInteraction, TextChannel } from "discord.js";
 
 
 export class RawGuildMemberAddListener extends Listener {
@@ -12,11 +12,11 @@ export class RawGuildMemberAddListener extends Listener {
         });
     }
 
-    public override async run(client: Client, interaction: Command.ChatInputCommandInteraction) {
+    public override async run(client: Client) {
         //Create message
         if (client.user) {
             const {username, id} = client.user!;
-            await interaction.reply(`Welcome @${client.user.tag}! Welcome to the ACM discord`)
+            await (client.channels.cache.get('greet') as TextChannel).send(`Hi @${client.user.tag}. Welcome to the ACM Discord!`)
         }
     }
 }
