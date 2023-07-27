633e450d5d504f2e4ebf504ec743b339bd92faf3
vinr567 <vinr567@gmail.com>
2023-07-27 16:03:57 -0700

    Modified permission to ADMIN ONLY and removed unused imports

--- src/commands/Bot/emitTest.ts
+++ src/commands/Bot/emitTest.ts
@@ -1,14 +1,15 @@
 import { Command } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
 import { isMessageInstance } from "@sapphire/discord.js-utilities";
-import { Client, GuildMember } from "discord.js";
+import { GuildMember } from "discord.js";
 import { EventEmitter } from "events";
-import { emit } from "process";
 
 
 @ApplyOptions<Command.Options>({
     name: 'emit-guild-add',
 	description: 'Emits GUILD_MEMBER_ADD to test welcome message',
+    aliases: ['kick'],
+    requiredUserPermissions: "Administrator"
 })
 export class PingCommand extends Command {
 	public override registerApplicationCommands(registry: Command.Registry) {
@@ -22,7 +23,7 @@ export class PingCommand extends Command {
         const msg = await interaction.reply({content: "emit", ephemeral: true, fetchReply: true});
         
         if (isMessageInstance(msg)) {
-            //Perform emmitance
+            //Emmits GUILD_MEMBER_ADD event to test the joinEventListener
             const emitter = new EventEmitter();
             await emitter.emit("GUILD_MEMBER_ADD", GuildMember);
             return await interaction.reply(`Event Emitted`);
