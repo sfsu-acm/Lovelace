76316e622c365cac333e62bfdb5a1231b1e93992
vinr567 <vinr567@gmail.com>
2023-07-28 15:07:55 -0700

    Implement logging for guild member removal/leaving
    leave server message to hardcoded logging channel
    
    hardcoded message to logger when member leaves or is removed from the discord
    
    simplied code in run to be one line
    
    simplied remove message code to one line as well
    
    Forgot to write in full name for remove enevnt in constructor
    
    Moved removeGuildMember listener to diff branch. Removed guild hardcoding for add member listener as well as changed formatting per prettier code formatter

--- src/listeners/guildMemberAddGreeting.ts
+++ src/listeners/guildMemberAddGreeting.ts
@@ -1,22 +1,21 @@
-import { Listener } from "@sapphire/framework";
-import {GuildMember, TextChannel } from "discord.js";
-
+import { Listener } from '@sapphire/framework';
+import { GuildMember, TextChannel } from 'discord.js';
 
 export class GuildMemberAddGreeting extends Listener {
-    public constructor(context: Listener.Context, options: Listener.Options) {
-        super(context, {
-            ...options,
-            event: 'guildMemberAdd',  
-            once: false
-        });
-    }
+	public constructor(context: Listener.Context, options: Listener.Options) {
+		super(context, {
+			...options,
+			event: 'guildMemberAdd',
+			once: false,
+		});
+	}
 
-    public override async run(member: GuildMember) {
-        //Only server ID needs to be changed when moving to ACM server
-        const systemChannelID = this.container.client.guilds.cache.get('1115435349171253360')!.systemChannelId!;
-        const channel = this.container.client.channels.cache.get(systemChannelID) as TextChannel
-        await channel.send(`Hi ${member.user}. Welcome to the ACM Discord Server!`)
-    }
+	public override async run(member: GuildMember) {
+		// Sending message to designated system channel using systemChannel property of Guild
+		await this.container.client.guilds.cache
+			.get('1115435349171253360')!
+			.systemChannel!.send(
+				`Hi ${member.user}. Welcome to the ACM Discord Server!`
+			);
+	}
 }
-
-
