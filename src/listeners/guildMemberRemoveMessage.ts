8f383cb46f7919a62dcd1e57de59594c5b323c58
OskoPoland <aoskotsky@gmail.com>
2023-07-28 23:15:06 -0700

    hardcoded message to logger when member leaves or is removed from the discord

--- src/listeners/guildMemberRemoveMessage.ts
+++ src/listeners/guildMemberRemoveMessage.ts
@@ -8,4 +8,9 @@ export class GuildMemberRemovalMessage extends Listener {
             event: 'guild'
         })
     }
+
+    public async run(member: GuildMember) {
+        const channel = this.container.client.channels.cache.get('1115435350572150807') as TextChannel
+        await channel.send(`${member.user} has left the server.`)
+    }
 }
