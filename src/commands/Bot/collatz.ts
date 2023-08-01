681615b30e4c4c0dc37296e5746e232a71730ac0
OskoPoland <aoskotsky@gmail.com>
2023-08-01 10:57:57 -0700

    Don't actually divide by 2 for even number

--- src/commands/Bot/collatz.ts
+++ src/commands/Bot/collatz.ts
@@ -41,7 +41,7 @@ export class CollatzCommand extends Command {
             if (input % 2 == 0) {
                 input /= 2
             } else {
-                input = (3 * input + 1) / 2
+                input = (3 * input + 1)
             }
             sequenceSet.add(input)
         }
